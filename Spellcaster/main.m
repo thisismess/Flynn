e7b91dc35682cc1f805bfb6e65a1c02a36fbc417
Brian William Wolter <brian@woltergroup.net>
2013-01-14 01:23:41 -0500

    Initial Commit

---
+++ Spellcaster/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Spellcaster
+//
+//  Created by Brian William Wolter on 1/14/13.
+//  Copyright (c) 2013 Mess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
