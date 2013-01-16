b3d145809ad91ca9114f9efe4d97877959908ad0
Brian William Wolter <brian@woltergroup.net>
2013-01-16 13:38:13 -0500

    ...

--- Web/assets/js/caster.js
+++ Web/assets/js/caster.js
@@ -1,12 +1,12 @@
 (function($){
-
+  
   function lpad(value, padding)
   {
       var zeroes = "0";
       for (var i = 0; i < padding; i++) { zeroes += "0"; }
       return (zeroes + value).slice(padding * -1);
   };
-
+  
   function base64ValueForCharAt(c, a) 
   {
     var b = c.charCodeAt(a);
@@ -31,11 +31,11 @@
   };
   
   $.fn.spellcaster = function(options){ 
-
+    
     return this.each(function(){
-
+      
       var ele = this;
-
+      
       // Defaults
       ele.options = $.extend({
         'fps': 24,
@@ -43,7 +43,7 @@
         'continous': false,
         'autoplay': true,
       }, options);
-
+      
       // Set all counters to 0
       ele.current_frame = 0;
       ele.keyframe_width = 0;
@@ -70,20 +70,18 @@
       ele.manifest 		= null;
       ele.manifest_file   = $(ele).attr('data-manifest');
       ele.image_directory = $(ele).attr('data-frame-directory');
-
+      
       ele.setup_canvas = function()
       {
         ele.canvas = $('<canvas></canvas>').appendTo(ele).attr({'width':parseInt($(ele).width(), 10), 'height':parseInt($(ele).height(), 10)});
         ele.actual_canvas = ele.canvas[0];
       };
-
+      
       ele.setup_debug_canvas = function()
       {
         ele.debug = $('<canvas class="debug"></canvas>').appendTo(ele).attr({'width':ele.source.width, 'height':ele.source.height});
-        //ele.debug.drawImage({ source:ele.source, x:0, y:0, width:ele.source.width, height:ele.source.height, fromCenter: false });
-
       };
-
+      
       ele.load_manifest = function()
       {
         $.getJSON(ele.manifest_file, function(data){
@@ -91,7 +89,7 @@
           ele.parse_header();
         });
       };
-
+      
       ele.parse_header = function()
       {
         if (ele.manifest === null) throw("No manifest loaded");
@@ -99,34 +97,33 @@
         ele.block_size = ele.manifest.blockSize;
         ele.load_frames();
       };
-
-
+      
       ele.load_frames = function()
       {
         if (ele.image_directory === undefined || ele.image_directory === null) throw("No image directory set.");
-
         for(var i = 0; i < ele.manifest.imagesRequired; i++)
         {
-          var src = ele.image_directory + (i+1) + '.png';
+          var src = ele.image_directory + 'spellcaster_00' + (i+1) + '.png';
           $(new Image()).attr('src', src).load(function(){
             ele.images.push(this);
             if (ele.images.length >= ele.manifest.imagesRequired && ele.options.autoplay === true) ele.start();
           });
         }
       };
-
+      
       ele.start = function()
       {
-        var src = ele.image_directory + 'keyframe.png';
+        var src = ele.image_directory + 'spellcaster_keyframe.png';
         $(new Image()).attr('src', src).load(function(){
           //ele.canvas.drawImage({ source: this, x:0, y:0, width:this.width, height:this.height, fromCenter:false });
+          ele.actual_canvas.getContext("2d").drawImage(this, 0, 0, this.width, this.height);
           ele.keyframe_width = this.width;
           ele.source = ele.images[ele.current_source];
           ele.setup_debug_canvas();
           ele.play();
         });
       };
-
+      
       ele.play = function()
       {
         ele.timeout = window.setTimeout(ele.next_frame, (ele.options.fps/1000));
@@ -151,10 +148,15 @@
           var dstOrigin = ele.originForPosition(position, ele.keyframe_width);
           var strip = Math.min(count, (ele.source.width - srcOrigin.x) / ele.block_size);
           context.clearRect(dstOrigin.x, dstOrigin.y, strip * ele.block_size, ele.block_size);
+          //context.fillStyle = "blue";
+          //context.fillRect(dstOrigin.x, dstOrigin.y, strip * ele.block_size, ele.block_size);
           context.drawImage(ele.source, srcOrigin.x, srcOrigin.y, strip * ele.block_size, ele.block_size, dstOrigin.x, dstOrigin.y, strip * ele.block_size, ele.block_size);
           ele.source_position += strip;
           position += strip;
           count -= strip;
+          // DEBUG
+          //ele.source_position += count;
+          //break;
         }
         
       };
@@ -180,8 +182,8 @@
 
       if (ele.options.autoplay) ele.setup_canvas();
       if (ele.options.autoplay) ele.load_manifest();
-
+      
     });
   };
-
+  
 })(jQuery);
